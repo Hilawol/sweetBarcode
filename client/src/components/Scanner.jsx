b6823251720205ddafb588eba37c89e7b28bbc81
Hila Kershenovich <37781805+Hilawol@users.noreply.github.com>
2021-11-12 12:55:54 +0200

    Added reader ref and code div

--- client/src/components/Scanner.jsx
+++ client/src/components/Scanner.jsx
@@ -1,41 +1,53 @@
-import React ,{useEffect} from 'react'
+import React ,{useEffect,useState,useRef} from 'react'
 import './scanner.css';
 import {Html5Qrcode} from "html5-qrcode";
 
 function Scanner() {
+
+  const [scanOn,setScanOn] =  useState(false);
+  const [scannedCode,setScannedCode] = useState(null);
+  const readerRef = useRef(null);
   
-  let html5QrCode ;
 
   useEffect(() => {
-    html5QrCode = new Html5Qrcode("reader");
+    const html5QrCode = new Html5Qrcode("reader");
+    readerRef.current = html5QrCode;
+    console.log(readerRef)
   }, [])
- 
 
+  const qrCodeSuccessCallback = (decodedText, decodedResult) =>{
 
-  const qrCodeSuccessCallback = (code) =>{
-
-    console.log("scanned:",code);
+    console.log("scanned:"+decodedText +" "+ decodedResult);
+    setScannedCode(decodedText);
 
   }
 
   const onStartScan = ()=>{
+    setScanOn(true);
     console.log("Start scan clicked ");
+  
     // If you want to prefer back camera
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-   html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+    readerRef.current.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
   }
 
   const stopScan = () =>{
-    html5QrCode.stop().then((ignore) => {
-      // QR Code scanning is stopped.
-    }).catch((err) => {
-      // Stop failed, handle it.
-    });
+    if (scanOn) {
+
+      readerRef.current.stop().then((ignore) => {
+        // QR Code scanning is stopped.
+      }).catch((err) => {
+        // Stop failed, handle it.
+      });
+
+      setScanOn(false);
+    }
   }
 
   return (
     <div className="main">
-      <div id="reader" width="600px"></div>
+      <div ref={readerRef} id="reader" width="600px"></div>
+      <div className="code">Scanned: {scannedCode}</div>
       <button className="btn" onClick = {onStartScan}>Scan</button>
       <button className="btn" onClick = {stopScan}>Stop Scan</button>
     </div>
